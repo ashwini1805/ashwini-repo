ece229b1c6be40a21366451912a4a62180ba52cb
ashwini <ashwini035cdac@gmail.com>
2024-06-04 17:36:09 +0530

    4th commit

--- src/main/java/com/example/Controller/UploadController.java
+++ src/main/java/com/example/Controller/UploadController.java
@@ -28,7 +28,7 @@ public class UploadController {
                                    RedirectAttributes redirectAttributes) {
 
         if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message1234", "Please select a file to upload");
+            redirectAttributes.addFlashAttribute("message12345", "Please select a file to upload");
             return "redirect:uploadStatus";
         }
 
